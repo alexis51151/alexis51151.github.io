70742a995210c717938adec0c89f8a2a0deda037
alexis51151 <44121643+alexis51151@users.noreply.github.com>
2025-08-12 01:39:15 +0000

    Deploying to gh-pages from @ alexis51151/alexis51151.github.io@5c5be24caa46aaded590a5d428590766e0537ad5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%6C%65%78%69%73.%6C%65%67%6C%61%75%6E%65%63@%72%69%63%65.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/alexis-leglaunec", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=yNesXXgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
